dfe35510586b773d4c87a99ed94a0f4d0c6b02fa
Alexander Gribowski <gribowski@jinr.ru>
2021-02-05 15:45:44 +0300

    Added macro for program testing

---
+++ macro/standTest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+run=816
+
+SOURCEDIR=/home/gribowski/soft/spdroot/spdroot
+BUILDDIR=$SOURCEDIR/../build
+
+rm -rf $BUILDDIR/*
+cd $BUILDDIR 
+cmake $SOURCEDIR &&
+make -j11 &&
+. config.sh &&
+cd $SOURCEDIR/macro
+root -b -q "standDataToRoot.C(\"mpd_run_Straw_stand_$run.data\")"
+root -b -q "standDigitsToHits.C($run, -1)"
+root -b -q "SiliconHitsAnalysisHist.C($run, 0)"
