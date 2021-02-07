714e0154da22adf8f4937f01f72e836acec928d5
Alexander Gribowski <gribowski@jinr.ru>
2021-02-07 13:20:26 +0300

    Moved common classes to a separate library

--- hitproducer/StandHitsProducerLinkDef.h
+++ hitproducer/StandHitsProducerLinkDef.h
@@ -5,8 +5,6 @@
 #pragma link off all functions;
 
 #pragma link C++ class StandHitsProducer+;
-#pragma link C++ class StandIOManager+;
-#pragma link C++ class StandSiliconGeoMapper+;
 
 #pragma link C++ class StandSiliconCluster+;
 #pragma link C++ class StandClustersContainer+;
