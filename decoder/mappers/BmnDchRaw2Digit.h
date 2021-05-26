fb5125c0373aebf1e72936458defbf4c8a569dd8
Alexander Gribowski <gribowski@jinr.ru>
2021-05-26 22:31:09 +0300

    clear BmnDchRaw2Digit

--- decoder/mappers/BmnDchRaw2Digit.h
+++ decoder/mappers/BmnDchRaw2Digit.h
@@ -1,57 +1,36 @@
 #ifndef BMNDCHRAW2DIGIT_H
 #define BMNDCHRAW2DIGIT_H
 
-#include "TString.h"
 #include "TClonesArray.h"
 #include "BmnTDCDigit.h"
-#include "BmnSyncDigit.h"
 #include "BmnDchDigit.h"
-#include <iostream>
 #include "Riostream.h"
 #include "BmnEnums.h"
 #include "BmnTQDCADCDigit.h"
 #include <cstdlib>
-#include <bitset>
-#include <map>
-#include "TMath.h"
 
 #include "db_structures.h"
 
 #define DCH_TDC_TYPE (0x10) //TDC64V
-#define DCH_TQDC_TYPE (0x56)
 
-#define HPTIMEBIN 0.02344
-#define INVHPTIMEBIN 42.6666
-#define KNBDCHANNELS 40
-#define ADC_CLOCK_TQDC16VS  8.0  // nano seconds
-#define ADC_CLOCK          12.0
-#define ADC_CLOCK_OLD      12.5
-#define TDC_CLOCK          24.0
-#define TDC_CLOCK_OLD      25.0
+#define N_TRIGGER_CHANNELS    5
+#define N_MAP_ENTRIES         2
 
-class BmnDchRaw2Digit {
+class BmnDchRaw2Digit
+{
 public:
     BmnDchRaw2Digit(Int_t period, Int_t run);
-    BmnDchRaw2Digit() {
-        fMap1 = NULL;
-    };
-    ~BmnDchRaw2Digit() {
-        if (fMap1) delete[] fMap1;
-    };
+    BmnDchRaw2Digit();
+    ~BmnDchRaw2Digit();
 
     void FillEvent(TClonesArray *tdc, TClonesArray *tqdc_tdc, TClonesArray *dch);
 
 private:
-
-    DchMapStructure* fMap1;
-
-    Int_t fEntriesInMap1; // member of entries in BD table for DCH1
+    DchMapStructure* fMap;
 
     Int_t GetChTDC64v(UInt_t tdc, UInt_t ch);
     BmnStatus FindInMap(BmnTDCDigit* dig, Double_t time_scint, TClonesArray* arr);
-
     BmnStatus ReadMapFromFile(Int_t period);
-    BmnStatus ReadMapFromDB(Int_t period, Int_t run);
 
     ClassDef(BmnDchRaw2Digit, 1);
 };
